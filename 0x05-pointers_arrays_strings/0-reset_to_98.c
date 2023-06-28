4bd74db8928b585714a613319fa9b5a5bdb7c216
OneCarat <133281538+OneCarat@users.noreply.github.com>
2023-06-28 21:02:02 +0300

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /*
-*reset_to_98-
-*takes the pointer to an int (*n) and updates it to 98
+*reset_to_98-takes the pointer to an int (*n) and updates it to 98
 */
 void reset_to_98(int *n)
 {
-    *n=98;
+    *n = 98;
 }
