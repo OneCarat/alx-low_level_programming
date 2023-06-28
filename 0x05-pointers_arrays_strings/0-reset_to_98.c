f98b6694407adc198bcb7344c0831a55f0573c91
OneCarat <133281538+OneCarat@users.noreply.github.com>
2023-06-28 21:15:44 +0300

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,6 @@
 /**
-*reset_to_98-replaces the pointer *n of an interger with 98
+*reset_to_98:replaces the pointer *n of an interger with 98
+*@n:pointer to interger n
 */
 void reset_to_98(int *n)
 {
