5d05329103e8c67f37a5d52b34c33c353be64f39
OneCarat <133281538+OneCarat@users.noreply.github.com>
2023-06-29 18:15:50 +0300

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+
+/* ... add prototypes for your other functions ... */
+
+#endif /* MAIN_H */
