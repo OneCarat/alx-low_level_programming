09fe4225830bdea344fed491fad6de33466a1247
OneCarat <133281538+OneCarat@users.noreply.github.com>
2023-06-29 19:48:06 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - Prints a string in reverse, followed by a new line.
+ * @s: Pointer to the string.
+ * Return: void.
+ */
+void print_rev(char *s)
+{
+int length = 0;
+while (s[length] != '\0')
+{
+length++;
+}
+for (int i = length; i > 0; i--)
+{
+putchar (s[i]);
+}
+putchar("\n"); 
+}
