1520912fcbe83b37679ed64f3d51938482fcb936
OneCarat <133281538+OneCarat@users.noreply.github.com>
2023-06-29 17:48:03 +0300

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,12 @@
 /**
  * _strlen - calculates the length of a string
- * @*s: pointer to the string
- * 
+ * @s: pointer to the string
  * Return: length of the string
 */
 int _strlen(char *s)
 {
 int length =0;
-while (*s != '\0')
+while (*s ! = '\0')
 {
 length++;
 s++;
