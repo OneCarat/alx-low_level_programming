380acfe85de4de95816f0c38b5d96fe8679431bd
OneCarat <133281538+OneCarat@users.noreply.github.com>
2023-06-29 20:33:51 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,15 +7,15 @@
 void print_rev(char *s)
 {
 int length = 0;
-int i=length;
+int i = length;
 while (s[length] != '\0')
 {
 length++;
 }
-i=length;
+i = length;
 while (i != 0)
 {
-putchar (s[i-1]);
+putchar (s[i - 1]);
 i--;
 }
 putchar ('\n');
