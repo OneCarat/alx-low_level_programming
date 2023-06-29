d99d95e09980b825afec7e8491dc7a577a7e2a12
OneCarat <133281538+OneCarat@users.noreply.github.com>
2023-06-29 18:53:56 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,7 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
-int putchar (*);
+int putchar (char);
 
 /* ... add prototypes for your other functions ... */
 
